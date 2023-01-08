55b730b793b9b4cda492bf5a9e6b91cd44a8e132
Edan-01 <108907378+Edan-01@users.noreply.github.com>
2023-01-08 01:05:49 -0500

    Upload get.js

---
+++ get.js
@@ -0,0 +1,3 @@
+$("body").load("https://edan-01.github.io/chatbotlastmen.github.io/index.html #chat");
+
+$("head").html('<link rel="stylesheet" href="https://edan-01.github.io/chatbotlastmen.github.io/style.css">');
